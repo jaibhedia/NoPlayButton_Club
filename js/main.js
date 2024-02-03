2c3bd68068572db674ecec7a08f37251337e6cfa
jaibhedia <jaibhedia@gmail.com>
2024-02-03 23:18:05 +0530

    updated html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-/*Navbar Open/Close toggle button* */
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+/*Navbar Open/Close toggle button */
+const hamburgerCheckbox = document.getElementById("hamburgerCheckbox");
+const navElement = document.querySelector('.nav');
+
+hamburgerCheckbox.addEventListener("change", function () {
+  navElement.classList.toggle("collapsible--expanded", this.checked);
+});
 
 /*Navbar Sticky*/
 window.onscroll = function () {
@@ -23,8 +23,8 @@ function myFunction() {
   }
 }
 
-/*Theme Toggle*/
-const checkbox = document.getElementById("checkbox");
+/* Theme Toggle */
+const checkbox = document.getElementById("toggletheme");
 const navItems = document.querySelectorAll(".nav__item");
 const heroTagline = document.querySelectorAll(".hero__tagline");
 const blockTexts = document.querySelectorAll(".block__text");
@@ -42,13 +42,17 @@ const banners = document.querySelectorAll(".banner");
 const btnPrimaryAll = document.querySelectorAll(".button--primary");
 const btnSecondaryAll = document.querySelectorAll(".button--secondary");
 const btnAccentTwoAll = document.querySelectorAll(".button--accent--two");
+const credits = document.querySelector('.credits');
+const text1 = document.querySelector('.text1');
+
 
 checkbox.addEventListener("change", () => {
+  toggleTheme();
   document.body.classList.toggle("dark");
   navItems.forEach((navItem) => {
     navItem.classList.toggle("text-light");
   });
-   heroTagline.forEach((navItem) => {
+  heroTagline.forEach((navItem) => {
     navItem.classList.toggle("text-light");
   });
   document.querySelector(".nav").classList.toggle("sticky-light");
@@ -105,3 +109,28 @@ checkbox.addEventListener("change", () => {
   });
   document.querySelector(".nav__toggler").classList.toggle("img-white");
 });
+
+function toggleTheme() {
+  const root = document.documentElement;
+
+  if (checkbox.checked) {
+    // Dark theme
+    root.style.setProperty('--bb8-bg', '#000');
+    root.style.setProperty('--accent', '#fff');
+    credits.classList.add('text-light');
+    text1.classList.add('text-light');
+  } else {
+    // Light theme
+    root.style.setProperty('--bb8-bg', '#fff');
+    root.style.setProperty('--accent', '#de7d2f');
+    credits.classList.remove('text-light');
+    text1.classList.remove('text-light');
+  }
+}
+
+
+
+
+
+
+//============================
